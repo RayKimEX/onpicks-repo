205b4aa722f426ea6c7f496a39ec993e8c55c7c4
Ray Kim <ray.kim@mojostudio.io>
2018-08-27 18:41:27 +0900

    structure angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './core/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// import { AuthEffects } from './auth/store/auth.effects';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     HttpClientModule,
+//     AppRoutingModule,
+//     SharedModule,
+//     ShoppingListModule,
+//     AuthModule,
+//     CoreModule,
+//     StoreModule.forRoot(reducers),
+//     EffectsModule.forRoot([AuthEffects]),
+//     StoreRouterConnectingModule,
+//     !environment.production ? StoreDevtoolsModule.instrument() : []
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
