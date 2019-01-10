679067f501761288254f96dd1cd6e6dfae1f3339
Ray Kim <ray.kim@mojostudio.io>
2019-01-10 11:43:23 +0900

    test For IE trying 4times

--- src/app/shops/pages/index/p/components/index/pure/p-reviews/p-reviews.component.ts
+++ src/app/shops/pages/index/p/components/index/pure/p-reviews/p-reviews.component.ts
@@ -104,7 +104,7 @@ export class PReviewsComponent implements AfterViewInit {
   }
 
   voteReviews() {
-    this.pDataService.voteReviewsData().subscribe( v => console.log(v));
+    // this.pDataService.voteReviewsData().subscribe( v => console.log(v));
   }
 
 
