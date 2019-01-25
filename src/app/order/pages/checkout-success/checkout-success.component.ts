912914094c40677884c02a6f703277783e103543
Ray Kim <ray.kim@mojostudio.io>
2019-01-25 14:47:58 +0900

    checkout-success gst

--- src/app/order/pages/checkout-success/checkout-success.component.ts
+++ src/app/order/pages/checkout-success/checkout-success.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'onpicks-checkout-success',
   templateUrl: './checkout-success.component.html',
-  styleUrls: ['./checkout-success.component.scss']
+  styleUrls: ['./checkout-success.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
-export class CheckoutSuccessComponent implements OnInit {
+export class CheckoutSuccessComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  queryParams$;
+
+  orderCode;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.queryParams$ = this.route.queryParams
+      .subscribe((val: {order_code}) => {
+        this.orderCode = val.order_code;
+        console.log('checkout-success param' + this.orderCode);
+      });
+  }
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy(){
+    this.queryParams$.unsubscribe();
   }
 
 }
