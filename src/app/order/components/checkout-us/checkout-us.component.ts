cb330a7040c68707a017c0ddaba4862ef411f2fd
jade <jade.joo@mojostudio.io>
2019-07-17 19:36:20 +0900

    checkout-us.component에서 scrollHandler 추가

--- src/app/order/components/checkout-us/checkout-us.component.ts
+++ src/app/order/components/checkout-us/checkout-us.component.ts
@@ -32,6 +32,8 @@ export class CheckoutUsComponent implements OnInit, AfterViewInit {
 
   @ViewChild('paypalPayment') paypalPayment;
 
+  private footerHeight: number;
+  private windowInnerHeight: number;
 
   constructor(
     @Inject( STATE_LIST ) public stateList,
@@ -407,6 +409,19 @@ export class CheckoutUsComponent implements OnInit, AfterViewInit {
       this.errorStatus &= ~this.INVALID_PHONE_NUMBER;
     } else {
       this.errorStatus |= this.INVALID_PHONE_NUMBER;
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.windowInnerHeight = window.innerHeight;
+  }
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event) {
+    const checkoutRightMenu = (document.querySelector('.checkout-right__menu') as HTMLElement);
+    const bodyHeight = (document.querySelector('body') as HTMLElement).offsetHeight;
+    const scrollTop = event.target.scrollingElement.scrollTop;
+    if (scrollTop + this.windowInnerHeight < bodyHeight - this.footerHeight ){
+      checkoutRightMenu.style.top = (scrollTop) + 'px';
     }
   }
 
