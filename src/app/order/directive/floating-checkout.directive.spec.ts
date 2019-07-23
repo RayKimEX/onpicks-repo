2937920973a86e0abea683b44c9661b85564ddee
jade <jade.joo@mojostudio.io>
2019-07-23 14:00:58 +0900

    체크아웃 메뉴 스크롤 시 따라다니게 변경

--- src/app/order/directive/floating-checkout.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FloatingCheckoutDirective } from './floating-checkout.directive';
-
-describe('FloatingCheckoutDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FloatingCheckoutDirective();
-    expect(directive).toBeTruthy();
-  });
-});
