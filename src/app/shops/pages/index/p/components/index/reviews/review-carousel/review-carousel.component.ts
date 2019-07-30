d197d2b618cac33cf262c1bd7a11c349d4e78301
Ray Kim <ray.kim@mojostudio.io>
2019-07-30 12:28:44 +0900

    added review carousel

---
+++ src/app/shops/pages/index/p/components/index/reviews/review-carousel/review-carousel.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit, ChangeDetectionStrategy, Input, HostListener, Renderer2, ChangeDetectorRef, ViewChild, ViewChildren, AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'onpicks-review-carousel',
+  templateUrl: './review-carousel.component.html',
+  styleUrls: ['./review-carousel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ReviewCarouselComponent implements OnInit, AfterViewInit {
+  @Input('imageList') imageList;
+  @ViewChild('container') container;
+  @ViewChildren('itemList') itemList;
+
+  imageIndex = 0;
+  containerWidth = 0;
+  capturedTranslateX = 0;
+
+  constructor(
+    private renderer: Renderer2,
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  hello(event) {
+    this.containerWidth = parseInt(getComputedStyle(this.container.nativeElement).width, 10);
+    this.capturedTranslateX  = ( this.containerWidth ) * this.imageIndex;
+    this.renderer.setStyle(this.container.nativeElement, 'transition', 'x');
+    this.renderer.setStyle(this.container.nativeElement, 'transform', 'translateX(-' + this.capturedTranslateX  + 'px)');
+  }
+
+  ngAfterViewInit() {
+    this.containerWidth = parseInt(getComputedStyle(this.container.nativeElement).width, 10);
+    this.capturedTranslateX  = ( this.containerWidth ) * this.imageIndex;
+  }
+
+
+  nextButton() {
+    if ( this.imageIndex < this.imageList.length - 1) {
+      this.imageIndex++;
+      this.capturedTranslateX  = ( this.containerWidth) * this.imageIndex;
+      this.renderer.setStyle(this.container.nativeElement, 'transition', 'transform .5s ease-in-out 0s');
+      this.renderer.setStyle(this.container.nativeElement, 'transform', 'translateX(-' + this.capturedTranslateX  + 'px)');
+
+    }
+    this.cd.markForCheck();
+  }
+
+  prevButton() {
+    console.log(this.imageIndex);
+
+    if ( this.imageIndex !== 0) {
+      this.imageIndex--;
+      this.capturedTranslateX  = ( this.containerWidth) * this.imageIndex;
+      this.renderer.setStyle(this.container.nativeElement, 'transition', 'transform .5s ease-in-out 0s');
+      this.renderer.setStyle(this.container.nativeElement, 'transform', 'translateX(-' + this.capturedTranslateX + 'px)');
+    }
+    this.cd.markForCheck();
+
+  }
+}
